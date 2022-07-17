16ef595b22415f116c00768a9a822664ea2eaff5
karadagokan <okankrdag@gmail.com>
2022-07-17 17:38:20 +0300

    Okan, 1 ,  Düzeltmeler yapıldı.

--- js/clock.js
+++ js/clock.js
@@ -1,20 +1,21 @@
 let userName;
-let timer=null;
+let timer;
 let setClock;
-while(true){
-    userName=prompt("Hoşgeldiniz Lütfen İsminizi Giriniz: ");
-    if(userName)
+
+while (true) {
+    userName = prompt("Hoşgeldiniz Lütfen İsminizi Giriniz: ");
+    if (userName)
         break;
 }
 
-document.getElementById('myName').innerText=userName;
+document.getElementById('myName').innerText = userName;
 
-setClock=document.getElementById('myClock');
+setClock = document.getElementById('myClock');
 
 timer = setInterval(function () {
-    let date=new Date();
-    setClock.innerText=`${date.toLocaleTimeString()} ${date.toLocaleDateString('tr-TR',{ weekday: 'long' })}`;
-  }, 100);
+    let date = new Date();
+    setClock.innerText = `${date.toLocaleTimeString()} ${date.toLocaleDateString('tr-TR', {weekday: 'long'})}`;
+}, 100);
 
 
 
